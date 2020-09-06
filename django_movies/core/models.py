6e3c6d4fd4d4d99c01454abd15fbd563800408d6
barto <bartosz.marmolowski@gmail.com>
2020-09-06 10:02:54 +0200

    Age limit choices

--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -4,7 +4,20 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Genre(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    age_limit = models.IntegerField(null=True, blank=True, validators=[MaxValueValidator(18), MinValueValidator(0)])
+    AGE_LIMIT_CHOICES = [
+        (3, 3),
+        (7, 7),
+        (13, 13),
+        (16, 16),
+        (18, 18),
+        (21, 21)
+    ]
+    age_limit = models.IntegerField(
+        null=True,
+        blank=True,
+        validators=[MaxValueValidator(18), MinValueValidator(0)],
+        choices=AGE_LIMIT_CHOICES
+    )
 
     def __str__(self):
         return self.name
@@ -19,7 +32,7 @@ class Movie(models.Model):
     released = models.DateField(null=True)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    genre = models.ForeignKey(Genre, null=True, on_delete=models.DO_NOTHING)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'{self.title} from {self.released}'
