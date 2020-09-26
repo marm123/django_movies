7e232247e7f466b10b0554244a807bed3c4ab61b
barto <bartosz.marmolowski@gmail.com>
2020-09-26 14:00:00 +0200

    Add logout/password change functionality

--- django_movies/accounts/urls.py
+++ django_movies/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accounts.views import SubmittableLoginView
+from accounts.views import SubmittableLoginView, SubmittablePasswordChangeView, SuccessMessagedLogoutView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', SubmittableLoginView.as_view(), name='login')
+    path('login/', SubmittableLoginView.as_view(), name='login'),
+    path('logout/', SuccessMessagedLogoutView.as_view(), name='logout'),
+    path('passwordchange/', SubmittablePasswordChangeView.as_view(), name='passchange')
 ]
